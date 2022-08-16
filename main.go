2095872f7cae5aaf2c3670628ca7e7d2152b0752
i-ates <i-ates@protonmail.com>
2022-08-16 23:00:42 +0300

    calculation created

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	calculatorsapcomv1alpha1 "sap.com/homework/api/v1alpha1"
+	"sap.com/homework/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -88,6 +89,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.CalculationReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Calculation")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
